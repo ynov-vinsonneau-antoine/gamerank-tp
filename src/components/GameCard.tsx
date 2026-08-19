ae9fc00ff72eadaa035c66377555bdea4e3efccd
AntoineP2 <antoine.vinsonneau34@gmail.com>
2026-08-19 17:29:39 +0200

    fix style

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,13 +2,11 @@ import type { GameCardType } from "../types/game";
 
 const GameCard = (game: GameCardType) => {
   return (
-    <div className="flex items-center gap-4 rounded-xl bg-slate-800 p-4 transition-colors hover:bg-slate-700">
-      <span className="grid h-10 w-10 place-items-center rounded-lg bg-violet-500 font-bold text-slate-900">
-        {game.note}
-      </span>
+    <div className="flex items-center gap-3">
+      <span className="">{game.note}</span>
       <div>
         <h2 className="font-semibold text-slate-50">{game.name}</h2>
-        <p className="text-sm text-slate-400">
+        <p className="text-sm text-white">
           {game.studio} · {game.releaseDate}
         </p>
       </div>
